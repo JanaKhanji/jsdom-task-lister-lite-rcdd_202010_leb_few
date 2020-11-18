40421854a53741a61ec5b114118281ae86fda61a
Jana Khanji <jana.khanji@ieee.org>
2020-11-18 14:15:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    //form and relevant input fields
+ 
   const form = document.getElementById("create-task-form");
-  const task = document.getElementById("new-task-description");
-
-
-  //attach event listeners
-  newTaskForm.addEventListener("submit", AddTask);
+  form.addEventListener("submit", AddTask);
 });
 
-const createNewTask = event => {
+const AddTask = event => {
   event.preventDefault();
-  //stop form from trying to submit
-  const newTaskDescription = document.getElementById("new-task-description");
+  const task = document.getElementById("new-task-description");
   const newTask = document.createElement("li");
   newTask.innerText = newTaskDescription.value;
 
