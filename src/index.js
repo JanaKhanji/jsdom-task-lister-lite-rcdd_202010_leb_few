f40ead14f31e2a48c5727ff979555068109946a0
Jana Khanji <jana.khanji@ieee.org>
2020-11-18 14:13:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const newTaskDescription = document.getElementById("new-task-description");
   const newTaskPriority = document.getElementById("new-task-priority");
 
-  //ul where new tasks will live on the DOM
-  const newTaskUl = document.getElementById("tasks");
 
   //attach event listeners
-  newTaskForm.addEventListener("submit", createNewTask);
+  newTaskForm.addEventListener("submit", AddTask);
 });
 
 const createNewTask = event => {
